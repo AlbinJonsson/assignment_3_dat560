24bb6d0dcb4e1010123ce0864aebf14e9b80bd11
Hadi <dinemail.se>
2026-02-28 23:45:14 +0100

    add comments

--- as2-petfeeder/src/test/java/petfeeder/FoodContainerTest.java
+++ as2-petfeeder/src/test/java/petfeeder/FoodContainerTest.java
@@ -14,9 +14,10 @@ class FoodContainerTest {
 
     @BeforeEach
     void setUp() throws MealPlanException {
+        // Initialize a new container before each test
         foodContainer = new FoodContainer();
 
-
+        // Create a valid meal plan with fixed amounts
         mealPlan = new MealPlan();
         mealPlan.setAmtWetFood("5");
         mealPlan.setAmtKibble("5");
@@ -25,38 +26,43 @@ class FoodContainerTest {
 
     }
 
-// TODO Treats method!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    /** Verifies default amount of treats */
     @Test
     void getTreats_returnValue(){
         assertEquals(15, foodContainer.getTreats());
     }
+    /** Verifies that positive value updates correctly */
     @Test
     void setTreats_update_positiveValue() {
         foodContainer.setTreats(20);
         assertEquals(20, foodContainer.getTreats());
     }
+    /** Verifies that negative value updates correctly */
     @Test
     void setTreats_update_negativeValue() {
         foodContainer.setTreats(-5);
         assertNotEquals(-5, foodContainer.getTreats());
     }
+    /** Verifies that zero is handled correctly */
     @Test
     void setTreats_update_nullValue() {
         foodContainer.setTreats(0);
         assertEquals(0, foodContainer.getTreats());
     }
+    /** Verifies successful addition of valid positive amount as string input*/
     @Test
     void addTreats_addPositive_value() throws Exception{
         foodContainer.addTreats("5");
         assertEquals(20, foodContainer.getTreats());
     }
-
+    /** Verifies successful addition of valid negative amount as string input*/
     @Test
     void addTreats_addNegative_value() throws Exception{
         assertThrows(FoodStockException.class, ()->{
             foodContainer.addTreats("-5");
         });
     }
+    /** Verifies exception when input is not numeric */
     @Test
     void addTreats_addString_value() throws Exception{
         assertThrows(FoodStockException.class, ()->{
@@ -64,39 +70,43 @@ class FoodContainerTest {
         });
     }
 
-
-    // TODO Kibble method!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    /** Verifies default amount of kibble */
     @Test
     void getKibble_returnValue() {
         assertEquals(15, foodContainer.getKibble());
     }
-
+    /** Verifies that positive value updates correctly */
     @Test
     void setKibble_update_positiveValue() {
         foodContainer.setKibble(25);
         assertEquals(25, foodContainer.getKibble());
     }
+    /** Verifies that negative value updates correctly */
     @Test
     void setKibble_update_negativeValue() {
         foodContainer.setTreats(-5);
         assertNotEquals(-5, foodContainer.getKibble());
     }
+    /** Verifies that zero is handled correctly */
     @Test
     void setKibble_update_nullValue() {
         foodContainer.setTreats(0);
         assertEquals(0, foodContainer.getTreats());
     }
+    /** Verifies successful addition of valid positive amount as string input*/
     @Test
     void addKibble_addPositive_value() throws Exception{
         foodContainer.addKibble("10");
         assertEquals(25,foodContainer.getKibble());
     }
+    /** Verifies successful addition of valid negative amount as string input*/
     @Test
     void addKibble_addNegative_value() throws Exception{
         assertThrows(FoodStockException.class, ()->{
             foodContainer.addKibble("-10");
         });
     }
+    /** Verifies exception when input is not numeric */
     @Test
     void addKibble_addString_value() throws Exception{
         assertThrows(FoodStockException.class, ()->{
@@ -104,37 +114,43 @@ class FoodContainerTest {
         });
     }
 
-    // TODO Water method!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    /** Verifies default amount of water */
     @Test
     void getWater_returnValue() {
         assertEquals(15, foodContainer.getWater());
     }
+    /** Verifies that positive value updates correctly */
     @Test
     void setWater_update_positiveValue() {
         foodContainer.setWater(5);
         assertEquals(5,foodContainer.getWater());
     }
+    /** Verifies that negative value updates correctly */
     @Test
     void setWater_update_negativeValue() {
         foodContainer.setTreats(-5);
         assertNotEquals(-5, foodContainer.getTreats());
     }
+    /** Verifies that zero is handled correctly */
     @Test
     void setWater_update_nullValue() {
         foodContainer.setTreats(0);
         assertEquals(0, foodContainer.getTreats());
     }
+    /** Verifies successful addition of valid positive amount as string input*/
     @Test
     void addWater_addPositive_value() throws Exception{
         foodContainer.addWater("4");
         assertEquals(19,foodContainer.getWater());
     }
+    /** Verifies successful addition of valid negative amount as string input*/
     @Test
     void addWater_addNegative_value() throws Exception{
         assertThrows(FoodStockException.class,()-> {
             foodContainer.addWater("-5");
         });
     }
+    /** Verifies exception when input is not numeric */
     @Test
     void addWater_addString_value() throws Exception{
         assertThrows(FoodStockException.class, ()->{
@@ -142,26 +158,30 @@ class FoodContainerTest {
         });
     }
 
-    // TODO WetFood method!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    /** Verifies default amount of wetFood */
     @Test
     void getWetFood_returnValue() {
         assertEquals(15, foodContainer.getWetFood());
     }
+    /** Verifies that positive value updates correctly */
     @Test
     void setWetFood_update_positiveValue() {
         foodContainer.setWetFood(1);
         assertEquals(1, foodContainer.getWetFood());
     }
+    /** Verifies that negative value updates correctly */
     @Test
     void setWetFood_update_negativeValue() {
         foodContainer.setTreats(-5);
         assertNotEquals(-5, foodContainer.getTreats());
     }
+    /** Verifies that zero is handled correctly */
     @Test
     void setWetFood_update_nullValue() {
         foodContainer.setTreats(0);
         assertEquals(0, foodContainer.getTreats());
     }
+    /** Verifies successful addition of valid positive amount as string input*/
     @Test
     void addWetFood_addPositive_value() throws Exception{
         foodContainer.addWetFood("5");
@@ -177,6 +197,7 @@ class FoodContainerTest {
      */
     // this should be like this
 
+    /** Verifies successful addition of valid negative amount as string input*/
     @Test
     void addWetFood_addNegative_value() throws Exception{
         assertThrows(FoodStockException.class, ()-> {
@@ -190,6 +211,8 @@ class FoodContainerTest {
            assertEquals(10, foodContainer.getWetFood());
        }
      */
+
+    /** Verifies exception when input is not numeric */
     @Test
     void addWetFood_addString_value() throws Exception{
         assertThrows(FoodStockException.class, ()->{
@@ -197,33 +220,35 @@ class FoodContainerTest {
         });
     }
 
-    // TODO enoughIngredients !!!!!!!!!!!!!!!!!!!
-
+    /** Verifies that container has sufficient food for meal plan*/
     @Test
     void sufficientFoodComparedToMealPlan () {
         assertTrue(foodContainer.enoughIngredients(mealPlan));
 
     }
 
-
+    /** Verifies detection of insufficient wetFood */
     @Test
     void sufficientFoodComparedToMealPlan_WetFood_false () {
         foodContainer.setWetFood(1);
         assertFalse(foodContainer.enoughIngredients(mealPlan));
 
     }
+    /** Verifies detection of insufficient kibble */
     @Test
     void sufficientFoodComparedToMealPlan_Kibble_false () {
         foodContainer.setKibble(1);
         assertFalse(foodContainer.enoughIngredients(mealPlan));
 
     }
+    /** Verifies detection of insufficient water */
     @Test
     void sufficientFoodComparedToMealPlan_Water_false () {
         foodContainer.setWater(1);
         assertFalse(foodContainer.enoughIngredients(mealPlan));
 
     }
+    /** Verifies detection of insufficient treats */
     @Test
     void sufficientFoodComparedToMealPlan_Treats_false () {
         foodContainer.setTreats(1);
@@ -231,8 +256,7 @@ class FoodContainerTest {
 
     }
 
-    // TODO useIngredients !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+    /** Verifies that ingredients are deducted when sufficient */
     @Test
     void useIngredients_remove_from_container_true() {
         boolean res = foodContainer.useIngredients(mealPlan);
@@ -244,6 +268,7 @@ class FoodContainerTest {
         assertEquals(10, foodContainer.getWetFood());
 
     }
+    /** Verifies that no invalid deduction occurs when insufficient */
     @Test
     void useIngredients_remove_from_container_false() {
         foodContainer.setTreats(4);
@@ -252,7 +277,7 @@ class FoodContainerTest {
         assertNotEquals(-1, foodContainer.getTreats());
     }
 
-    //TODO test for toString() method
+    /** Verifies formatted string representation of container state */
     @Test
     void toString_returnsCorrectFormat() {
         // By default, FoodContainer has 15 units of everything
